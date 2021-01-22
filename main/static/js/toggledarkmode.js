306edb1c28ff89a512a58fa710aa5dde4b023962
Abdullah Alam <abdullahalam598@gmail.com>
2021-01-22 13:33:36 +0530

    Dark mode working: full commit

---
+++ main/static/js/toggledarkmode.js
@@ -0,0 +1,61 @@
+function toggleDarkMode(){
+    
+    // GET DARK MODE SHEET 
+    var darksheet = document.getElementById('darkmodesheet');
+
+    // SWITCH
+    darksheet.disabled = !darksheet.disabled
+
+    // REMEMBER
+    document.cookie = 'darksheet=' + String(darksheet.disabled) + '; path=/; SameSite=Lax'
+
+}
+
+function getCookieVal(key){
+
+    cookiestring = document.cookie
+    keyvals = null
+
+    // IF COOKIE STRING CONTAINS ;
+    if(cookiestring.includes('; ')){
+        cookies = cookiestring.split('; ')
+        for (i = 0; i < cookies.length - 1; i++){
+            cookie = cookies[i]
+            keyvals = cookie.split('=')
+
+            if (keyvals[0] == key){
+                return(keyvals[1])
+            }
+        }
+    } else {
+        keyvals = cookiestring.split('=')
+
+        if (keyvals[0] == key){
+            return(keyvals[1])
+        }
+    }
+
+    return(null)
+
+
+}
+
+function updateDarkMode(){
+
+    // darksheet COOKIE VAL
+    let darksheetcookie = getCookieVal('darksheet')
+    console.log(darksheetcookie !== null)
+
+    // GET DARK MODE SHEET 
+    var darksheet = document.getElementById('darkmodesheet');
+
+    // SWITCH
+    if(darksheetcookie !== null){ // DO NOTHING ON FIRST LOAD
+        darksheet.disabled = (getCookieVal('darksheet') === 'true')
+    }    
+    
+}
+
+
+// UPDATE DARK MODE ON LOAD
+updateDarkMode()
